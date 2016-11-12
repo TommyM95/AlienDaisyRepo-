9b55381882017f52eca89a859fe308dfe7d371b9
Tommy McGIll <mcgillt95@gmail.com>
2016-11-12 22:34:42 +0000

    414

---
+++ ItemSystem/Item System/Assets/imGekko/ItemSystem/Scripts/Editor/ISQualityDatabaseEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+namespace imGekko.ItemSystem.Editor {
+        public class ISQualityDatabaseEditor : EditorWindow
+        {
+            ISQualityDatabase db;
+
+            const string DATABASE_PATH = @"Assests/Database/GekkoQualityDatabase.asset";
+
+            [MenuItem("Gekko/Database/Quality Editor %#i")]
+            public static void Init() {
+                ISQualityDatabaseEditor window = EditorWindow.GetWindow<ISQualityDatabaseEditor>();
+                window.minSize = new Vector2(400, 300);
+                window.title = "Quality Database";
+                window.Show();
+
+            }
+
+     
+       }
+
+
+
+    void OnEnable() {
+
+        db = AssetDatabase.LoadAss
+
+        if (db == null) {
+        db = ScriptableObject.CreateInstance<ISQualityDatabase>();
+        AssetDatabase.CreateAsset(db, DATABASE_PATH);
+        AssetDatabase.SaveAssets();
+        }
+    }
+}
